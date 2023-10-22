edc9d6a07f149756b89ba8b610c657356da27621
pejor76 <pejorcr@gmail.com>
2023-10-22 18:12:02 +0200

    Login Fix

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -28,8 +28,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.awt.event.ActionEvent;
+
 public class Login extends JFrame {
-	//declaracion de variables
+	// declaracion de variables
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -38,8 +39,9 @@ public class Login extends JFrame {
 	private ConexionMySQL conex;
 	private UserController us;
 	private ArrayList<User> userList;
-	private ArrayList<Specialist>speciaList;
+	private ArrayList<Specialist> speciaList;
 	private SpecialistController sp;
+
 	/**
 	 * Launch the application.
 	 */
@@ -60,19 +62,20 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
-		//declaracion de las conexiones
+		// declaracion de las conexiones
 		this.conex = new ConexionMySQL();
-		 conex.conectar();
-		us=new UserController(conex);
-		sp=new SpecialistController(conex);
-		//traemos todos los datos de la tabla especialista a nuestro ArrayList del modelo Specialist
+		conex.conectar();
+		us = new UserController(conex);
+		sp = new SpecialistController(conex);
+		// traemos todos los datos de la tabla especialista a nuestro ArrayList del
+		// modelo Specialist
 		try {
-			speciaList=sp.getAllSpecialist();
-			userList=us.getAllUsers();
-		}catch(SQLException e) {
+			speciaList = sp.getAllSpecialist();
+			userList = us.getAllUsers();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -80,76 +83,75 @@ public class Login extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblLogin = new JLabel("Login");
 		lblLogin.setBounds(182, 23, 46, 14);
 		contentPane.add(lblLogin);
-		
+
 		JLabel lblDNI = new JLabel("DNI");
 		lblDNI.setBounds(94, 74, 62, 14);
 		contentPane.add(lblDNI);
-		
+
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setBounds(94, 129, 46, 14);
 		contentPane.add(lblPassword);
-		
+
 		tfDNI = new JTextField();
 		tfDNI.setBounds(240, 71, 75, 20);
 		contentPane.add(tfDNI);
 		tfDNI.setColumns(10);
-		
+
 		tfPassword = new JTextField();
 		tfPassword.setBounds(240, 126, 75, 20);
 		contentPane.add(tfPassword);
 		tfPassword.setColumns(10);
-		
+
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String username = tfDNI.getText();
 				String password = tfPassword.getText();
-				boolean aux2=true;
-				
-				for(User x:userList) {
-					//si coinciden nombre y contraseña con alguno de los usuarios
-					if((x.getDni().toString().equals(username))&&(x.getPass().toString().equals(password))&&aux2) {
-						//esta dado de alta
-						
-						
-						if(x.isState()) {
-							
-							for(Specialist s:speciaList) {//interactua por todos los especistas existentes
-								if(x.getDni().toString().equalsIgnoreCase(s.getDni().toString())) {
-									
-									//en el caso de que el usuario este dentro de los especialistas del centro dental
-									if(s.getId_speciality()==1) {
-										//se abre la pantalla de admin
+				boolean aux2 = true;
+
+				for (User x : userList) {
+					// si coinciden nombre y contraseña con alguno de los usuarios
+					if ((x.getDni().toString().equals(username)) && (x.getPass().toString().equals(password)) && aux2) {
+						// esta dado de alta
+
+						aux2 = false;
+						if (x.isState()) {
+
+							for (Specialist s : speciaList) {// interactua por todos los especistas existentes
+								if (x.getDni().toString().equalsIgnoreCase(s.getDni().toString())) {
+
+									// en el caso de que el usuario este dentro de los especialistas del centro
+									// dental
+									if (s.getId_speciality() == 1) {
+										// se abre la pantalla de admin
 										System.out.println("adsijdashbasdhi");
-										User aux=x;
-										pAdmin pa=new pAdmin();
-										
+										User aux = x;
+										pAdmin pa = new pAdmin();
+
 										pa.setVisible(true);
 										dispose();
-									}else {//si no es admin es doctor
-										//declaracion de la pantalla doctor
+									} else {// si no es admin es doctor
+											// declaracion de la pantalla doctor
 										dispose();
 									}
-								}else {
-									//si no encuentra el usuario en los especialistas
-									JOptionPane.showMessageDialog(btnLogin,"Su usuario no esta registrado como especialista", "Error",JOptionPane.ERROR_MESSAGE);
-									break;
 								}
 							}
-						}else {//esta dado de baja
-							JOptionPane.showMessageDialog(btnLogin,"Cuidado","Este usuario ya no es válido",JOptionPane.WARNING_MESSAGE);
+						} else {// esta dado de baja
+							JOptionPane.showMessageDialog(btnLogin, "Cuidado", "Este usuario ya no es válido",
+									JOptionPane.WARNING_MESSAGE);
 							break;
 						}
 					}
 				}
-				if(aux2 == true) {
-					JOptionPane.showMessageDialog(btnLogin,"Su usuario o contraseña no coincide.\n Intentelo de nuevo", "Error",JOptionPane.ERROR_MESSAGE);
+				if (aux2 == true) {
+					JOptionPane.showMessageDialog(btnLogin, "Su usuario o contraseña no coincide.\n Intentelo de nuevo",
+							"Error", JOptionPane.ERROR_MESSAGE);
 				}
-			
+
 			}
 		});
 		btnLogin.setBounds(155, 206, 89, 23);
