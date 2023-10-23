15f6765decbb1fe8485458e3438cc7836b326cf3
avg0020 <avg0020@alu.medac.es>
2023-10-23 14:30:18 +0200

    Pantallas Admin
    Ya está toda la estructura básica de las pantallas de Admin.

--- src/Vista/AdminClinic.java
+++ src/Vista/AdminClinic.java
@@ -2,8 +2,6 @@ package Vista;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -12,15 +10,11 @@ import java.awt.event.MouseListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.SoftBevelBorder;
 
-public class AdminUsers extends JFrame {
+public class AdminClinic extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -32,7 +26,7 @@ public class AdminUsers extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AdminUsers frame = new AdminUsers();
+					AdminClinic frame = new AdminClinic();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -44,7 +38,7 @@ public class AdminUsers extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public AdminUsers() {
+	public AdminClinic() {
 		// -------------------- JFrame --------------------
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -54,258 +48,259 @@ public class AdminUsers extends JFrame {
 		this.setUndecorated(true);
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		// -------------------- Componentes Gráficos --------------------
 		// Panel del Menú
 		JPanel menuPane = new JPanel();
-		menuPane.setBackground(new Color(148,220,219));
+		menuPane.setBackground(new Color(148, 220, 219));
 		menuPane.setBounds(0, 0, 135, 1080);
 		menuPane.setLayout(null);
-		
-		//Label del Logo del Menú
+
+		// Label del Logo del Menú
 		JLabel lblLogo = new JLabel();
-		//lblLogo.setBorder(new LineBorder(new Color(0, 0, 0)));
+		// lblLogo.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblLogo.setBounds(0, 0, 135, 135);
 		lblLogo.setIcon(new ImageIcon(getClass().getResource("/Resources/images/logoMenu.png")));
-		
-		//Botón de citas  
+
+		// Botón de citas
 		JButton btnAppointment = new JButton();
-		btnAppointment.setBackground(new Color(148,220,219));
+		btnAppointment.setBackground(new Color(148, 220, 219));
 		btnAppointment.setBounds(0, 135, 135, 135);
 		btnAppointment.setBorderPainted(false);
 		btnAppointment.setIcon(new ImageIcon(getClass().getResource("/Resources/images/calendarGrey.png")));
-		btnAppointment.addMouseListener(new MouseListener() {	
+		btnAppointment.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
-				btnAppointment.setBackground(new Color(148,220,219));
+				btnAppointment.setBackground(new Color(148, 220, 219));
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				btnAppointment.setBackground(new Color(31,192,191));
+				btnAppointment.setBackground(new Color(31, 192, 191));
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
 		btnAppointment.setToolTipText("Módulo de citas");
-		
-		//Botón de usuarios
+
+		// Botón de usuarios
 		JButton btnUsers = new JButton();
-		btnUsers.setBackground(new Color(238,238,238));
+		btnUsers.setBackground(new Color(148, 220, 219));
 		btnUsers.setBounds(0, 270, 135, 135);
 		btnUsers.setBorderPainted(false);
-		btnUsers.setIcon(new ImageIcon(getClass().getResource("/Resources/images/users.png")));
+		btnUsers.setIcon(new ImageIcon(getClass().getResource("/Resources/images/usersGrey.png")));
 		btnUsers.setToolTipText("Módulo de usuarios");
-		
-		//Botón de Pacientes
+		btnUsers.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnUsers.setBackground(new Color(148, 220, 219));
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnUsers.setBackground(new Color(31, 192, 191));
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		// Botón de Pacientes
 		JButton btnCustomers = new JButton();
-		btnCustomers.setBackground(new Color(148,220,219));
+		btnCustomers.setBackground(new Color(148, 220, 219));
 		btnCustomers.setBounds(0, 405, 135, 135);
 		btnCustomers.setBorderPainted(false);
 		btnCustomers.setIcon(new ImageIcon(getClass().getResource("/Resources/images/customersGrey.png")));
 		btnCustomers.setToolTipText("Módulo de pacientes");
-		btnCustomers.addMouseListener(new MouseListener() {	
+		btnCustomers.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
-				btnCustomers.setBackground(new Color(148,220,219));
+				btnCustomers.setBackground(new Color(148, 220, 219));
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				btnCustomers.setBackground(new Color(31,192,191));
+				btnCustomers.setBackground(new Color(31, 192, 191));
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
-		//Botón de Inventario
+
+		// Botón de Inventario
 		JButton btnStock = new JButton();
-		btnStock.setBackground(new Color(148,220,219));
+		btnStock.setBackground(new Color(148, 220, 219));
 		btnStock.setBounds(0, 540, 135, 135);
 		btnStock.setBorderPainted(false);
 		btnStock.setIcon(new ImageIcon(getClass().getResource("/Resources/images/stockGrey.png")));
 		btnStock.setToolTipText("Módulo de materiales");
-		btnStock.addMouseListener(new MouseListener() {	
+		btnStock.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
-				btnStock.setBackground(new Color(148,220,219));
+				btnStock.setBackground(new Color(148, 220, 219));
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				btnStock.setBackground(new Color(31,192,191));
+				btnStock.setBackground(new Color(31, 192, 191));
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
-		//Botón de Tratamientos y Especialidades
+
+		// Botón de Tratamientos y Especialidades
 		JButton btnClinic = new JButton();
-		btnClinic.setBackground(new Color(148,220,219));
+		btnClinic.setBackground(new Color(238, 238, 238));
 		btnClinic.setBounds(0, 675, 135, 135);
 		btnClinic.setBorderPainted(false);
-		btnClinic.setIcon(new ImageIcon(getClass().getResource("/Resources/images/clinicGrey.png")));
+		btnClinic.setIcon(new ImageIcon(getClass().getResource("/Resources/images/clinic.png")));
 		btnClinic.setToolTipText("Módulo clínico");
-		btnClinic.addMouseListener(new MouseListener() {	
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				btnClinic.setBackground(new Color(148,220,219));
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				btnClinic.setBackground(new Color(31,192,191));
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 		
-		//Botón del Módulo economico
+
+		// Botón del Módulo economico
 		JButton btnPayments = new JButton();
-		btnPayments.setBackground(new Color(148,220,219));
-		btnPayments.setBounds(0,810,135,135);
+		btnPayments.setBackground(new Color(148, 220, 219));
+		btnPayments.setBounds(0, 810, 135, 135);
 		btnPayments.setBorderPainted(false);
 		btnPayments.setIcon(new ImageIcon(getClass().getResource("/Resources/images/paymentsGrey.png")));
 		btnPayments.setToolTipText("Módulo Económico");
-		btnPayments.addMouseListener(new MouseListener() {	
+		btnPayments.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
-				btnPayments.setBackground(new Color(148,220,219));
+				btnPayments.setBackground(new Color(148, 220, 219));
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				btnPayments.setBackground(new Color(31,192,191));
+				btnPayments.setBackground(new Color(31, 192, 191));
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
-		//Botón de Salir
+
+		// Botón de Salir
 		JButton btnClose = new JButton();
-		btnClose.setBackground(new Color(148,220,219));
-		btnClose.setBounds(0,945,135,135);
+		btnClose.setBackground(new Color(148, 220, 219));
+		btnClose.setBounds(0, 945, 135, 135);
 		btnClose.setIcon(new ImageIcon(getClass().getResource("/Resources/images/logoutMenuGrey.png")));
 		btnClose.setToolTipText("Módulo Económico");
 		btnClose.setBorderPainted(false);
-		btnClose.addMouseListener(new MouseListener() {	
+		btnClose.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
-				btnClose.setBackground(new Color(148,220,219));
+				btnClose.setBackground(new Color(148, 220, 219));
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				btnClose.setBackground(new Color(31,192,191));
+				btnClose.setBackground(new Color(31, 192, 191));
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
+
 		// -------------------- Lógica --------------------
-		//Acción de salir
+		// Acción de salir
 		btnClose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		
+
 		// -------------------- Adiciones a los paneles --------------------
 		contentPane.add(menuPane);
 		menuPane.add(lblLogo);
@@ -316,14 +311,6 @@ public class AdminUsers extends JFrame {
 		menuPane.add(btnClinic);
 		menuPane.add(btnPayments);
 		menuPane.add(btnClose);
-		
-		/*
-		JInternalFrame internalFrame = new JInternalFrame("New JInternalFrame");
-		internalFrame.setBorder(null);
-		internalFrame.setBounds(185, 135, 1685, 810);
-		internalFrame.setBorder(new LineBorder(new Color(0, 0, 0)));
-		contentPane.add(internalFrame);
-		internalFrame.setVisible(true);
-		*/
 	}
+
 }
