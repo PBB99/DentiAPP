1298032a820a9570012ebe5135515bd9b5a74ab3
pejor76 <pejorcr@gmail.com>
2023-11-10 19:32:45 +0100

    Tabla citas

--- src/Vista/PruebasCalendario.java
+++ src/Vista/PruebasCalendario.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 
@@ -22,7 +23,9 @@ import javax.swing.table.DefaultTableModel;
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
 
+import Controlador.CitasController;
 import Controlador.ConexionMySQL;
+import Modelo.Cita;
 import Modelo.User;
 
 import javax.swing.JTextField;
@@ -38,6 +41,8 @@ public class PruebasCalendario extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTable table;
+	private ConexionMySQL conexion;
+	
 
 	/**
 	 * Launch the application.
@@ -59,6 +64,8 @@ public class PruebasCalendario extends JFrame {
 	 * Create the frame.
 	 */
 	public PruebasCalendario() {
+		conexion = new ConexionMySQL();
+		conexion.conectar();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 638, 447);
 		contentPane = new JPanel();
@@ -105,34 +112,38 @@ public class PruebasCalendario extends JFrame {
 		Calendar fechaCalen = new GregorianCalendar();
 		DateFormat formateador= new SimpleDateFormat("dd/M/yy");
 		System.out.println(formateador.format(fechaCalen.getTime()));
-		ConexionMySQL conexion = new ConexionMySQL();
-		conexion.conectar();
-		String consulta = "Select * From byprip7xk9sybmhhq0jf.cita";
+		CitasController citaCont = new CitasController(conexion);
+		ArrayList<Cita> allCitas;
 		try {
-			ResultSet rset = conexion.ejecutarSelect(consulta);
+			allCitas = citaCont.getAllSpecialist();
+			for (int i = 9; i < 15; i++) {
+				modelo.insertRow(modelo.getRowCount(), new Object[] { i+":00", "" });
+			}
+			for (int i = 17; i < 20; i++) {
+				modelo.insertRow(modelo.getRowCount(), new Object[] { i+":00", "" });
+			}
+			table.setBounds(302, 77, 199, 196);
+			contentPane.add(table);
 			
-			while (rset.next()) {
-				Date dia = rset.getDate("fecha");
+			for (int i = 0; i < allCitas.size(); i++) {
+				System.out.println("saddasdas");
+				Date dia = allCitas.get(i).getFecha();
 				System.out.println(formateador.format(dia) + "------");
 				if(formateador.format(fechaCalen.getTime()).equals(formateador.format(dia))) {
-					System.out.println("oooooooooo");
+					for (int j = 0; j < table.getModel().getRowCount(); j++) {
+						if(table.getModel().getValueAt(j, 0).equals(allCitas.get(i).getHora())){
+							table.getModel().setValueAt("Texto de ejemplo", j, 1);
+						}
+					}
 				}
 			}
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
-		for (int i = 9; i < 15; i++) {
-			modelo.insertRow(modelo.getRowCount(), new Object[] { i+":00", "" });
-		}
-		for (int i = 17; i < 20; i++) {
-			modelo.insertRow(modelo.getRowCount(), new Object[] { i+":00", "" });
-		}
-		table.setBounds(302, 77, 199, 196);
-		contentPane.add(table);
+		
 		
 		//JTable table = new JTable();
 		
@@ -140,12 +151,35 @@ public class PruebasCalendario extends JFrame {
 			
 			@Override
 			public void propertyChange(PropertyChangeEvent evt) {
+				
 				// TODO Auto-generated method stub
-				System.out.println("asddasdsaasd");
-				Calendar c = (Calendar) evt.getNewValue();
-				System.out.println(c.get(Calendar.DAY_OF_MONTH));
-				System.out.println(c.get(Calendar.MONTH)+1);
-				System.out.println(c.get(Calendar.YEAR));
+				for (int j = 1; j < table.getModel().getRowCount(); j++) {
+					table.getModel().setValueAt("", j, 1);
+				}
+				
+				
+				Calendar fechaCal = (Calendar) evt.getNewValue();
+				CitasController citaCont = new CitasController(conexion);
+				ArrayList<Cita> allCitas;
+				try {
+					allCitas = citaCont.getAllSpecialist();
+					for (int i = 0; i < allCitas.size(); i++) {
+						Date dia = allCitas.get(i).getFecha();
+						
+						System.out.println(formateador.format(fechaCal.getTime()) + "------");
+						if(formateador.format(fechaCal.getTime()).equals(formateador.format(dia))) {
+							System.out.println("saddasdas");
+							for (int j = 0; j < table.getModel().getRowCount(); j++) {
+								if(table.getModel().getValueAt(j, 0).equals(allCitas.get(i).getHora())){
+									table.getModel().setValueAt("Texto de ejemplo", j, 1);
+								}
+							}
+						}
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
