bd565d5370a688768d6d1b0286dbe0280dc0a0ba
PBB99 <pablobb9924@gmail.com>
2023-11-12 12:25:10 +0100

    adasd

--- src/Vista/DChangePass.java
+++ src/Vista/DChangePass.java
@@ -11,36 +11,49 @@ import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
+
+import Controlador.ConexionMySQL;
+import Controlador.UserController;
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.CardLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class DChangePass extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	private JTextField TFoldPass;
-	private JTextField TFnewPass1;
-	private JTextField TFnewPass2;
+	private JPasswordField TFoldPass;
+	private JPasswordField TFnewPass1;
+	private JPasswordField TFnewPass2;
+	private boolean show=false;
+	private char contra='1';
+	private ConexionMySQL conexion;
+	private String usuario;
+	private UserController userController;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		try {
-			DChangePass dialog = new DChangePass();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		//try {
+//			DChangePass dialog = new DChangePass();
+//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//			dialog.setVisible(true);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+		//}
 	}
 
 	/**
 	 * Create the dialog.
 	 */
-	public DChangePass() {
+	public DChangePass(ConexionMySQL conexion,String usuario) {
+		this.conexion=conexion;
+		userController=new UserController(conexion);
 		//aparece en x:1360 y y:0, se extiende 560 pixeles  a la derecha y 1080 hacia abajo
 		setBounds(1360, 0, 560, 1080);
 		getContentPane().setLayout(null);
@@ -56,17 +69,41 @@ public class DChangePass extends JDialog {
 			buttonPane.setLayout(null);
 			{
 				JButton okButton = new JButton("CAMBIAR CONTRASEÑA");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						//String conttra=conexion.ejecutarSelect("Select contraseña from usuario a join specialista b on ")
+					}
+				});
 				okButton.setBounds(243, 0, 281, 37);
 				okButton.setBorderPainted(false);
 				okButton.setBackground(new Color(148, 220, 219));
 				okButton.setForeground(new Color(255, 255, 255));
-				okButton.setFont(new Font("Gentium Book Basic", Font.BOLD, 23));
+				okButton.setFont(new Font("Dialog", Font.BOLD, 20));
 				buttonPane.add(okButton);
 				okButton.setActionCommand("OK");
 				getRootPane().setDefaultButton(okButton);
 			}
 			
 			JButton Btoggle = new JButton("MOSTRAR");
+			Btoggle.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(show==false) {
+					 contra=TFoldPass.getEchoChar();
+						TFoldPass.setEchoChar((char)0);
+						TFnewPass1.setEchoChar((char)0);
+						TFnewPass2.setEchoChar((char)0);
+						Btoggle.setText("Ocultar");
+						show=true;
+					}else {
+						TFoldPass.setEchoChar((char)contra);
+						TFnewPass1.setEchoChar((char)contra);
+						TFnewPass2.setEchoChar((char)contra);
+						Btoggle.setText("Mostrar");
+						show=false;
+					}
+					
+				}
+			});
 			Btoggle.setBounds(9, 0, 224, 37);
 			Btoggle.setForeground(Color.WHITE);
 			Btoggle.setFont(new Font("Gentium Book Basic", Font.BOLD, 23));
@@ -114,5 +151,7 @@ public class DChangePass extends JDialog {
 		lnewPass2.setFont(new Font("Gentium Book Basic", Font.BOLD, 30));
 		lnewPass2.setBounds(49, 268, 379, 38);
 		getContentPane().add(lnewPass2);
+		
+		
 	}
 }
